871a0186093f306d16eaa28fa19508c57f6749cf
sunhy469 <2635254982@qq.com>
2024-06-28 09:33:37 +0800

    配置vite

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,18 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+
+  server: {
+    host: "localhost",
+    open: true,
+    port: 3000,
+
+    proxy:{
+      "/api1":{
+        target:"http://localhost:8080",
+        changeOrigin:true,
+        rewrite:(path)=>path.replace(/^\/api1/,"")
+      },
+    },
+  },
 })
