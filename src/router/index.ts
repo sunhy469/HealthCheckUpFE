c2ba85faf846d29be8dda8bd896c2ef10d372b24
duinshanghai <lianbiao2022@163.com>
2024-06-29 15:02:49 +0800

    登入页面+注册页面

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,9 @@
 import { createRouter, createWebHistory } from 'vue-router'
+
 const SigninView = () => import('../views/SigninView.vue')
 const MainView = () => import('../views/MainView.vue')
+const RegisterView = () => import('../views/RegisterView.vue')
+
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
     routes: [
@@ -12,12 +15,21 @@ const router = createRouter({
             meta: {
                 title: '登录'
             }
-        },{
+        },
+        {
             path: '/main',
             name: 'main',
             component: MainView,
             meta: {
                 title: '主页面'
+            }
+        },
+        {
+            path: '/register',
+            name: 'register',
+            component: RegisterView,
+            meta: {
+                title: '注册'
             }
         }
     ]
@@ -25,8 +37,11 @@ const router = createRouter({
 
 router.beforeEach((to, from, next) => {
     const currentUserString = sessionStorage.getItem('currentUser')
-    if (to.name !== 'signin' && currentUserString == null) next({ path: '/signin' })
-    else next()
+    if (to.name !== 'signin' && to.name !== 'register' && currentUserString == null) {
+        next({ path: '/signin' })
+    } else {
+        next()
+    }
     document.title = String(to.meta.title);
 })
 
--- src/views/SigninView.vue
+++ src/views/SigninView.vue
@@ -1,72 +1,175 @@
 <template>
-  <h2 class="login-title">我的登陆页面</h2>
-  <a-tabs class="mytabs" v-model:activeKey="activeKey">
-    <a-tab-pane key="1" tab="Tab 1">
-      <div class="login-container">
-        <a-form :model="formState" name="normal_login" class="login-form" @finish="onFinish"
-                @finishFailed="onFinishFailed">
-          <a-form-item label="账号" name="username"
-                       :rules="[{ required: true, message: '请输入你的账号!' }]">
-            <a-input v-model:value="formState.username">
-              <template #prefix>
-                <UserOutlined class="site-form-item-icon"/>
-              </template>
-            </a-input>
-          </a-form-item>
-
-          <a-form-item label="密码" name="password"
-                       :rules="[{ required: true, message: '请输入你的密码!' }]">
-            <a-input-password v-model:value="formState.password">
-              <template #prefix>
-                <LockOutlined class="site-form-item-icon"/>
-              </template>
-            </a-input-password>
-          </a-form-item>
-
-          <a-form-item>
-            <a-form-item name="remember" no-style>
-              <a-checkbox v-model:checked="formState.remember">记住我&nbsp;&nbsp;&nbsp;&nbsp;</a-checkbox>
+  <h2 class="login-title">登陆页面</h2>
+  <a-card style="width: 500px; height: 400px; position: relative; left: 33%">
+    <a-tabs centered class="mytabs" v-model:activeKey="activeKey">
+      <a-tab-pane key="1" tab="用户名登入">
+        <div class="login-container">
+          <a-form :model="formState" name="normal_login" class="login-form" @finish="onFinish"
+                  @finishFailed="onFinishFailed">
+            <a-form-item label="账号" name="username"
+                         :rules="[{ required: true, message: '请输入你的账号!' }]" @blur='checkMethodForUsername()'>
+              <a-input v-model:value="formState.username">
+                <template #prefix>
+                  <UserOutlined class="site-form-item-icon"/>
+                </template>
+              </a-input>
             </a-form-item>
-            <a class="login-form-forgot" href="">忘记密码</a>
-          </a-form-item>
 
-          <a-form-item>
-            <a-button :disabled="disabled" type="primary" html-type="submit" class="login-form-button">
-              登入
-            </a-button>
-            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-            <a-button type="primary" html-type="submit" class="login-form-button">
-              注册
-            </a-button>
-          </a-form-item>
-        </a-form>
-      </div>
-    </a-tab-pane>
+            <a-form-item label="密码" name="password"
+                         :rules="[{ required: true, message: '请输入你的密码!' }]">
+              <a-input-password v-model:value="formState.password">
+                <template #prefix>
+                  <LockOutlined class="site-form-item-icon"/>
+                </template>
+              </a-input-password>
+            </a-form-item>
 
-    <a-tab-pane key="3" tab="Tab 3">
-      1
-    </a-tab-pane>
-  </a-tabs>
+            <a-form-item>
+              <a-form-item name="remember" no-style>
+                <a-checkbox v-model:checked="formState.remember">记住我&nbsp;&nbsp;&nbsp;&nbsp;</a-checkbox>
+              </a-form-item>
+              <a class="login-form-forgot" href="">忘记密码</a>
+            </a-form-item>
 
+            <a-form-item class="mybutton">
+              <a-button :disabled="disabled1" type="primary"  @click="signinMethod" html-type="submit" class="login-form-button">
+                登入
+              </a-button>
+              &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+              <a-button type="primary" @click="goToRegister" html-type="submit" class="login-form-button">
+                注册
+              </a-button>
+            </a-form-item>
+
+          </a-form>
+        </div>
+      </a-tab-pane>
+
+      <a-tab-pane key="2" tab="手机号登入">
+        <div class="login-container">
+          <a-form :model="formState" name="phone_login" class="login-form" @finish="onFinish"
+                  @finishFailed="onFinishFailed">
+            <a-form-item label="手机号" name="phonenumber"
+                         :rules="[{ required: true, message: '请输入你的手机号!' }]" @blur='checkMethodForPhoneNumber()'>
+              <a-input v-model:value="formState.phonenumber">
+                <template #prefix>
+                  <UserOutlined class="site-form-item-icon"/>
+                </template>
+              </a-input>
+            </a-form-item>
+
+            <a-form-item label="验证码" name="verificationCode"
+                         :rules="[{ required: true, message: '请输入你的验证码!' }]" @blur='checkVerificationCode()'>
+              <a-row gutter="8">
+                <a-col span="15">
+                  <a-input v-model:value="formState.verificationCode"/>
+                </a-col>
+                <a-col span="9">
+                  <a-button @click="sendVerificationCode">发送验证码</a-button>
+                </a-col>
+              </a-row>
+            </a-form-item>
+            <a-form-item>
+              <a-form-item name="remember" no-style>
+                <a-checkbox v-model:checked="formState.remember">记住我&nbsp;&nbsp;&nbsp;&nbsp;</a-checkbox>
+              </a-form-item>
+            </a-form-item>
+            <a-form-item class="mybutton">
+              <a-button :disabled="disabled2" type="primary" html-type="submit" class="login-form-button">
+                登入
+              </a-button>
+              &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+              <a-button type="primary" html-type="submit" class="login-form-button">
+                注册
+              </a-button>
+            </a-form-item>
+          </a-form>
+        </div>
+      </a-tab-pane>
+    </a-tabs>
+  </a-card>
 </template>
+
 <script lang="ts" setup>
-import {reactive, computed} from 'vue';
-import {ref} from 'vue';
+import {reactive, computed, getCurrentInstance, ref} from 'vue';
+import { useRouter } from 'vue-router';
 
 const activeKey = ref('1');
 import {UserOutlined, LockOutlined} from '@ant-design/icons-vue';
 
+const goToRegister = () => {
+  router.push({ path: '/register' });
+};
+
+const { proxy } = getCurrentInstance();
+const checkMethodForUsername = async () => {
+  try {
+    let { data: { code, msg } } = await proxy.$axios.post("doctor/checkUsername", 'username=' + formState.username);
+    if (code == 1 && msg == "INVALID") {
+      proxy.$message.info(`无效的用户名。`);
+    }
+  } catch (error) {
+    proxy.$message.warning(`系统繁忙。请稍后。`);
+  }
+};
+
+const checkMethodForPhoneNumber = async () => {
+  try {
+    let { data: { code, msg } } = await proxy.$axios.post("doctor/checkPhoneNumber", 'phonenumber=' + formState.phonenumber);
+    if (code == 1 && msg == "INVALID") {
+      proxy.$message.info(`无效的手机号。`);
+    }
+  } catch (error) {
+    proxy.$message.warning(`系统繁忙。请稍后。`);
+  }
+};
+
+const router = useRouter();
+const signinMethod = async () => {
+  try {
+    let { data: { code, msg, entity } } = await proxy.$axios.post("doctor/signin", 'username=' + formState.username, 'password=' + formState.password);
+    if (code == 1 && msg == "SUCCESS") {
+      sessionStorage.setItem('currentUser', JSON.stringify(entity));
+      router.push({ path: '/main' });
+    } else {
+      proxy.$message.warning(`无效的用户名或密码。`);
+    }
+  } catch (error) {
+    proxy.$message.warning(`系统繁忙。请稍后。`);
+  }
+};
+
+const checkVerificationCode = async () => {
+  try {
+    let { data: { code, msg, entity } } = await proxy.$axios.post("doctor/checkVerificationCode", 'verificationCode=' + formState.verificationCode);
+    if (code == 1 && msg == "INVALID") {
+      proxy.$message.warning(`无效的验证码。`);
+    } else if (code == 1 && msg == "SUCCESS") {
+      sessionStorage.setItem('currentphonenumber', JSON.stringify(entity));
+      router.push({ path: '/main' });
+    }
+  } catch (error) {
+    proxy.$message.warning(`系统繁忙。请稍后。`);
+  }
+};
+
+
 interface FormState {
   username: string;
   password: string;
+  phonenumber: string;
+  verificationCode: string;
   remember: boolean;
 }
 
 const formState = reactive<FormState>({
   username: '',
   password: '',
+  phonenumber: '',
+  verificationCode: '',
   remember: true,
 });
+
 const onFinish = (values: any) => {
   console.log('Success:', values);
 };
@@ -74,20 +177,24 @@ const onFinish = (values: any) => {
 const onFinishFailed = (errorInfo: any) => {
   console.log('Failed:', errorInfo);
 };
-const disabled = computed(() => {
+
+const sendVerificationCode = () => {
+  console.log('发送验证码');
+  // 这里可以添加发送验证码的逻辑
+};
+
+const disabled1 = computed(() => {
   return !(formState.username && formState.password);
 });
-</script>
-<style scoped>
 
-.mytabs{
-  position: relative;
-  left: 40%;
-}
-.login-container {
-  position: absolute;
-  width: 100%;
-  height: 100%;
+const disabled2 = computed(() => {
+  return !(formState.phonenumber && formState.verificationCode);
+});
+</script>
+
+<style scoped>
+.mybutton {
+  text-align: center;
 }
 
 .login-title {
@@ -99,10 +206,9 @@ const disabled = computed(() => {
 }
 
 .login-form {
-  width: 565px;
-  height: 372px;
-  margin: 0 auto;
-  padding: 40px 110px;
+  margin-top: 30px;
+  margin-left: 50px;
+  margin-right: 50px;
 }
 
 .login-form-forgot {
@@ -110,6 +216,6 @@ const disabled = computed(() => {
 }
 
 .login-form-button {
-  width: 100%;
+  width: 45%;
 }
 </style>
